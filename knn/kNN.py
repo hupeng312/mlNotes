bba7d2779b2517b24d5d4a74541e87e6e78b90a2
hupeng <hupeng@hupengdeMacBook-Pro.local>
2018-12-09 03:06:32 +0800

    实现knn

---
+++ knn/kNN.py
@@ -0,0 +1,38 @@
+import numpy as np
+from math import sqrt
+from collections import Counter
+
+class KNNClassifier:
+
+    def __init__(self, k):
+        '''k 为knn的个数'''
+        self.k = k
+        self._X_train = None
+        self._y_train = None
+
+    def fit(self, X, y):
+        '''X 为样本 因为分类结果'''
+        assert X.shape[0] == y.shape[0], '样本和标签数量不一致'
+        assert self.k <= X.shape[0], '样本数量级必须大于指定的k'
+        self._X_train = X
+        self._y_train = y
+        return self
+
+    def predict(self, X_predict):
+        '''x 为预测结果'''
+        assert self._X_train is not None and self._y_train is not None, 'must fit before predict!'
+        assert X_predict.shape[1] == self._X_train.shape[1], '预测数据特征数必须等于训练数据特征数'
+
+        y_predict = [self._predict(x_predict) for x_predict in X_predict]
+        return np.array(y_predict)
+
+    def _predict(self, x):
+        '''实际预测算法'''
+        distances = [sqrt(np.sum((x_train - x) ** 2)) for x_train in self._X_train]
+        nearSet = np.argsort(distances)
+        topK_y = [self._y_train[i] for i in nearSet[:self.k]]
+        votes = Counter(topK_y)
+        return votes.most_common(1)[0][0]
+
+    def __repr__(self):
+        return "KNN(k=%d)" % self.k
---
+++ knn/kNN_classify.py
@@ -0,0 +1,16 @@
+import numpy as np
+from math import sqrt
+from collections import Counter
+
+def kNN_classify(k, X_train, y_train, x):
+    assert 1 <= k <= X_train.shape[0], 'k must be valid'
+    assert X_train.shape[0] == y_train.shape[0], '样本的和结果标签个数不等'
+    assert X_train.shape[1] == x.shape[0], "预测数据和训练数据结构不一致"
+
+    distances = [sqrt(np.sum((x_train - x) ** 2)) for x_train in X_train]
+    nearest = np.argsort(distances)
+
+    topK_y = [y_train[i] for i in nearest[:k]]
+    votes = Counter(topK_y)
+
+    return votes.most_common(1)[0][0]
